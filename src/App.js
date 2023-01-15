9c7f95ede3b2e1a4a88da2ae5ab2b842b7dd318f
Mariya Ivanova <m80ivanova@gmail.com>
2023-01-14 18:16:30 -0600

    Setting up Navbar 1.14.23

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import NucampLogo from "./app/assets/img/logo.png"
 import './App.css';
+
+/*The href="/" in the NavbarBrand below means that when someone clicks on the Logo they'll be taken to our root folder (the home page of our app) */
 
 function App() {
   return (
     <div className="App">
-     Hello Nucamp!
+      <Navbar dark color="success" sticky='top' expand="md">
+        <Container>
+          <NavbarBrand href="/">
+            <img src={NucampLogo} alt="nucamp logo"/>
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+      Let's get started!
     </div>
   );
 }
